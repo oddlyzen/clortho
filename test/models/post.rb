a046d51b7429d393b5138099b651e7e9be9d3200
Mark Coates <mark.coates@gmail.com>
2011-08-02 15:56:00 -0400

    Adding a couple extra illegal punctuation characters in the LIPSUM instance variable in Post. Tests still pass, w00t.

--- test/models/post.rb
+++ test/models/post.rb
@@ -13,8 +13,8 @@ class Post
   searchable :about, :exclude => [:verbs, :adjectives, 'lipsum'] # or options like :exclude.
   
   LIPSUM  = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus porttitor, ipsum a commodo aliquet, 
-             velit ligula porttitor eros, sit amet consequat purus massa sit amet quam. Aliquam tempus magna faucibus 
-             lacus ultricies rhoncus. In ut metus purus, at venenatis est. Donec vel elementum turpis. Nullam feugiat 
+             velit ligula porttitor eros, sit amet consequat purus massa sit amet quam. Aliquam: tempus magna faucibus 
+             lacus ultricies rhoncus. In ut metus purus, at venenatis est. Donec vel--elementum turpis. Nullam feugiat 
              massa quis elit egestas."
   
 end
